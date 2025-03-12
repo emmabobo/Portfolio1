c9364d6f62b0f36f77ee7ab15beb86f07f1b7ad0
emmanbobo <olawalealawode@gmail.com>
2025-03-12 15:51:57 +0100

    Implemting Navbar Header not functional

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../assets/LogoB.png';
-
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logoW.png'
+import logoText from '../assets/logoWhite.png'
 
 function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
-    <nav className='container bg-gray-800 text-white' >
-      <div className=' mx-auto flex justify-between items-center py-4'>
-        <div>
-          <img className='' src={Logo} alt='Logo'/>
-        </div>
+    <div className='bg-gray-800 text-white '>
+      <nav className='mx-auto flex max-w-7xl items-center justify-between p-4 md:px-8  lg:p-6'>
         
-        <div>
-          <button className='hidden md:block'>Download</button>
+        {/* Logo section */}
+        <div class="flex lg:flex-1">
+          <Link to="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">La_Drogo</span>
+            <img className="h-8 w-auto lg:hidden " src={logo} alt="" />
+            <img className='h-5 w-auto hidden lg:block' src={logoText} alt='' />
+          </Link>
         </div>
 
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-
-        <div>
-          <button>Download</button>
+        {/* Hamburger-menu */}
+        <div class="flex md:hidden" onClick={() => console.log('clicked')}>
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Open main menu</span>
+            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
         </div>
-        
-      </div>
-    </nav>
+
+        {/* Menu Items*/}
+        <div className='hidden md:flex  md:gap-x-12'>
+          <Link to='/' className='active:bg-black hover:text-red-500' >Home</Link>
+          <Link to='/about' className='active:bg-black hover:text-red-500'>About</Link>
+          <Link to='/services' className='active:bg-black hover:text-red-500'>Services</Link>
+          <Link to='/contact' className='active:bg-black hover:text-red-500'>Contact</Link>
+        </div>
+
+
+      </nav>
+    </div>
   )
 }
 
-export default Header;
+export default Header
+
+
