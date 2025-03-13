4c636928b877050280de68a16546c9e9e0209948
emmanbobo <olawalealawode@gmail.com>
2025-03-13 16:09:56 +0100

    Implement hero section and fix navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,15 +8,26 @@ function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   const handleMobileMenu = () => {
-    setMobileMenuOpen(!mobileMenuOpen)
-  }
+    // Get the element by its ID (replace 'yourElementId' with the actual ID)
+    const elementId = document.getElementById('navLink');
+    if (mobileMenuOpen){
+      elementId.classList.add('hidden');
+    }
+    else{
+      elementId.classList.remove('hidden');
+    }
+
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+
 
   return (
     <div className='absolute top-0 right-0 left-0 bg-gray-800 text-white '>
       <nav className='mx-auto flex max-w-7xl items-center justify-between p-4 md:px-8  lg:p-6'>
         
         {/* Logo section */}
-        <div class="flex lg:flex-1">
+        <div onClick={handleMobileMenu} className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">La_Drogo</span>
             <img className="h-8 w-auto lg:hidden " src={logo} alt="" />
@@ -25,21 +36,21 @@ function Header() {
         </div>
 
         {/* Hamburger-menu */}
-        <div class="flex md:hidden" onClick={handleMobileMenu}>
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-            <span class="sr-only">Open main menu</span>
-            <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" aria-hidden="true" data-slot="icon">
+        <div className=" flex md:hidden" >
+          <button type="button" onClick={handleMobileMenu} class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span className="sr-only">Open main menu</span>
+            <svg className="size-8" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" aria-hidden="true" data-slot="icon">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
           </button>
         </div>
 
         {/* Menu Items*/}
-        <div className='hidden md:flex  md:gap-x-12'>
-          <Link to='/' className='active:bg-black hover:text-red-500' >Home</Link>
-          <Link to='/about' className='active:bg-black hover:text-red-500'>About</Link>
-          <Link to='/services' className='active:bg-black hover:text-red-500'>Services</Link>
-          <Link to='/contact' className='active:bg-black hover:text-red-500'>Contact</Link>
+        <div id='navLink' onClick={handleMobileMenu} className=' absolute top-15 left-0 right-0 h-fit items-center rounded-md  p-8 text-5xl flex flex-col gap-8 bg-gray-700 md:top-0 md:flex md:relative md:flex-row md:text-2xl  md:p-2 md:pl-6 md:rounded-tl-full'>
+          <Link to='/' onClick={handleMobileMenu} className='active:bg-black hover:text-red-500' >Home</Link>
+          <Link to='/about' onClick={handleMobileMenu} className='active:bg-black hover:text-red-500'>About</Link>
+          <Link to='/services' onClick={handleMobileMenu} className='active:bg-black hover:text-red-500'>Services</Link>
+          <Link to='/contact' onClick={handleMobileMenu} className='active:bg-black hover:text-red-500'>Contact</Link>
         </div>
 
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import  '../assets/hero.png'
+
 
 
 
 function Hero() {
   return (
-    <div className='bg-gray-800 text-white '>
-      <div className=' bg-gray-600 mx-auto flex max-w-7xl h-screen items-center justify-between p-4 md:px-8  lg:p-32' >
-        <div className='bg-red-400 '>
-          <h1 className='text-4xl '>Welcome to La_Drogo</h1>
+    <div className='bg-black text-white '>
+      <div className='bg-hero bg-cover mx-auto flex max-w-7xl h-screen items-center justify-between p-4 md:px-8  lg:p-32' >
+        <div className=''>
+          <h1 className='text-4xl '>Emmanuel</h1>
+          <h1 className='text-4xl '>Alawode</h1>
           <p className='text-lg'>Code. Create. Connect.</p>
+          <p className='text-lg'>I am a full stack developer</p>
+        
           <Link to='/contact' className='bg-white text-black p-2 rounded-md'>Try For Free</Link>
         </div>
 
@@ -17,8 +22,8 @@ function Hero() {
 
         </div>
 
-        <div className='bg-yellow-400 absolute'>
-          //imge here
+        <div className='bg-yellow-400 '>
+          
         </div>
       </div>
     </div>
